2ee75e0a2c30b9824a18b2bc193c84f0421747d4
Venus <iweixiaopeng@gmail.com>
2025-11-02 10:59:08 +0800

    fix: use fixed UTC+8 timezone for datetime tests to ensure consistency

--- datetime/common_test.go
+++ datetime/common_test.go
@@ -2,8 +2,13 @@ package datetime
 
 import "time"
 
+var (
+	// testLocation is the fixed timezone used for testing (UTC+8)
+	testLocation = time.FixedZone("UTC+8", 8*60*60)
+)
+
 func testTime(year int, month time.Month, day, hour, min, sec int) time.Time {
-	return time.Date(year, month, day, hour, min, sec, 0, time.Local)
+	return time.Date(year, month, day, hour, min, sec, 0, testLocation)
 }
 
 func testTimeUTC(year int, month time.Month, day, hour, min, sec int) time.Time {
