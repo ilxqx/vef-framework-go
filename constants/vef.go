f01eb2967247a26ce23c7299c035d34c2c5332a0
Venus <iweixiaopeng@gmail.com>
2025-11-15 01:04:10 +0800

    chore: bump version to v0.11.2

--- constants/vef.go
+++ constants/vef.go
@@ -3,5 +3,5 @@ package constants
 const (
 	// VEFVersion represents the current version of the VEF Framework.
 	// This version number follows semantic versioning (semver) format.
-	VEFVersion = "v0.11.1"
+	VEFVersion = "v0.11.2"
 )
