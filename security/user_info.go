f170448cd95578e0477f5daed5aa03e407530cb8
Venus <iweixiaopeng@gmail.com>
2025-11-19 15:16:17 +0800

    style: apply formatting and omitempty tags

--- security/user_info.go
+++ security/user_info.go
@@ -5,10 +5,8 @@ import "github.com/guregu/null/v6"
 type Gender string
 
 const (
-	GenderMale Gender = "male"
-
-	GenderFemale Gender = "female"
-
+	GenderMale    Gender = "male"
+	GenderFemale  Gender = "female"
 	GenderUnknown Gender = "unknown"
 )
 
@@ -16,40 +14,27 @@ type UserMenuType string
 
 const (
 	UserMenuTypeDirectory UserMenuType = "directory"
-
-	UserMenuTypeMenu UserMenuType = "menu"
-
-	UserMenuTypeView UserMenuType = "view"
-
+	UserMenuTypeMenu      UserMenuType = "menu"
+	UserMenuTypeView      UserMenuType = "view"
 	UserMenuTypeDashboard UserMenuType = "dashboard"
-
-	UserMenuTypeReport UserMenuType = "report"
+	UserMenuTypeReport    UserMenuType = "report"
 )
 
 type UserMenu struct {
-	Type UserMenuType `json:"type"`
-
-	Path string `json:"path"`
-
-	Name string `json:"name"`
-
-	Icon null.String `json:"icon"`
-
-	Meta map[string]any `json:"metadata"`
-
-	Children []UserMenu `json:"children"`
+	Type     UserMenuType   `json:"type"`
+	Path     string         `json:"path"`
+	Name     string         `json:"name"`
+	Icon     null.String    `json:"icon"`
+	Meta     map[string]any `json:"metadata,omitempty"`
+	Children []UserMenu     `json:"children,omitempty"`
 }
 
 type UserInfo struct {
-	Id string `json:"id"`
-
-	Name string `json:"name"`
-
-	Gender Gender `json:"gender"`
-
-	Avatar null.String `json:"avatar"`
-
-	PermTokens []string `json:"permTokens"`
-
-	Menus []UserMenu `json:"menus"`
+	Id         string      `json:"id"`
+	Name       string      `json:"name"`
+	Gender     Gender      `json:"gender"`
+	Avatar     null.String `json:"avatar"`
+	PermTokens []string    `json:"permTokens"`
+	Menus      []UserMenu  `json:"menus"`
+	Details    any         `json:"details,omitempty"`
 }
