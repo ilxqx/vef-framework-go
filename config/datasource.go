f1c35298b991063bf21fba28742ff266c57ef4d2
Venus <iweixiaopeng@gmail.com>
2025-12-26 17:12:19 +0800

    style: apply formatting and add SQL Guard config option
    - Remove redundant field comments in config structs
    - Add EnableSqlGuard option to datasource config
    - Apply consistent code formatting across config, password, and database providers

--- config/datasource.go
+++ config/datasource.go
@@ -3,12 +3,13 @@ package config
 import "github.com/ilxqx/vef-framework-go/constants"
 
 type DatasourceConfig struct {
-	Type     constants.DbType `config:"type"`
-	Path     string           `config:"path"`
-	Host     string           `config:"host"`
-	Port     uint16           `config:"port"`
-	User     string           `config:"user"`
-	Password string           `config:"password"`
-	Database string           `config:"database"`
-	Schema   string           `config:"schema"`
+	Type           constants.DbType `config:"type"`
+	Path           string           `config:"path"`
+	Host           string           `config:"host"`
+	Port           uint16           `config:"port"`
+	User           string           `config:"user"`
+	Password       string           `config:"password"`
+	Database       string           `config:"database"`
+	Schema         string           `config:"schema"`
+	EnableSqlGuard bool             `config:"enable_sql_guard"`
 }
